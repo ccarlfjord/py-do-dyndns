6e41a587348ede323cae1ea02216968fb249d708
Charles Carlfjord <gauz93@gmail.com>
2016-12-13 19:23:48 +0100

    stuff

--- py-do-dyndns.py
+++ py-do-dyndns.py
@@ -1,8 +1,16 @@
 #!/bin/python3
 
+import logging
+import sys
 import json
 import requests
 
-r = requests.get('https://ipinfo.io')
-json = r.json()
-print(json)
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+class GetIpAddr(object):
+
+    r = requests.get('https://ipinfo.io')
+    json = r.json()
+    # print(json)
+    
+
+x = GetIpAddr()
