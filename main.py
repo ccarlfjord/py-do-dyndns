ad53ae8c808706904038624bd48bc7afd29b9f9f
Charles Carlfjord <gauz93@gmail.com>
2016-12-14 22:21:26 +0100

    class for fetching ip-addr and class for reading DO key

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import logging
@@ -7,28 +7,30 @@ import json
 import requests
 import time
 
+#API_KEY = 'API_KEY'
+
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-class GetIpAdress(object):
-    
+class FetchIp(object):
+
     def __init__(self):
-        # self.GetIpAdress = GetIpAdress
-        self.show_me = GetIpAdress.show_me()
-        # self.get_ip = GetIpAdress.get_ip()
-        self.DigitalOcean = GetIpAdress.DigitalOcean()
-    
-    def get_ip():
-        adressApi = 'https://api.ipify.org'
-        f = requests.get(adressApi).text
-        return f
+        self.api = 'https://api.ipify.org'
+        self.data = requests.get(self.api).text
 
-    def DigitalOcean():
-        apiKey = open('./API_KEY', 'r').readline().rstrip()
-        print(apiKey)
+class DoApiKey(object):
+    def __init__(self, *arg):
+        self.file = 'API_KEY'
+        self.data = open(self.file, 'r').readline().rstrip()
         
-    def show_me():
-        print(GetIpAdress.get_ip())
-        #print(GetIpAdress.DigitalOcean())
+x = FetchIp().data
+f = DoApiKey().data
 
-GetIpAdress()
+y = 0
+while y < 3:
+    print(x)
+    print(f)
+    y +=1
+    # def DigitalOcean():
+    #     apiKey = open('./API_KEY', 'r').readline().rstrip()
+    #     return apiKey
